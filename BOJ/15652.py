a0e98b5ec47abaf45d6350f3db82aba67235ab1d
junyng <pom0894@naver.com>
2021-11-11 01:55:50 +0900

    N과 M (4)

---
+++ BOJ/15652.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split(" "))
+c = [False] * (n+1)
+a = [0] * m
+
+def go(index, start, n, m):
+    if index == m:
+        print(' '.join(map(str, a)))
+        return
+    
+    for i in range(start, n+1):
+        c[i] = True
+        a[index] = i
+        go(index + 1, i, n, m)
+        c[i] = False
+
+go(0, 1, n, m)
